0272a5fac57775774731784aecc88ebca0052358
macklinhrw <macklin202@gmail.com>
2022-05-18 23:52:58 -0700

    init commit

---
+++ src/popup/Popup.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import '@src/index.css'
+  import clj from "@src/popup/clojure"
+  console.log(clj.hello())
+  clj.printHello();
+</script>
+
+<main>
+  <div>
+    <p>Hello Typescript!</p>
+  </div>
+</main>
+
+<style>
+</style>
